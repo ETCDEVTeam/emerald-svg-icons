df7ab5f4887381f0f0fe3c912c745eab36cd8397
ia <isaac.ardis@gmail.com>
2018-03-17 21:23:21 -0500

    problem: each rendered icon gets a newline... readme requires scrolling
    solution: remove linebreaks after each icon tag

--- icons-to-readme.sh
+++ icons-to-readme.sh
@@ -13,12 +13,18 @@ for folder in "${folders[@]}"; do
 	echo "# $folder" >> $writefile
 
 	# loop through all icons in folder
+
+	# this will be one string per icons directory to concatenate all the icon
+	# html tags to avoid line breaks after each tag
+	iconsbatch=""
 	for icon in "./$folder/"*.svg; do
 		echo "icon: $icon"
 		if [[ -f $icon ]]; then
 			# <img src="icons/add.svg" width="24" height="24"/>
 			# echo "![$file]($file)" >> README.md
-			echo "<img src='$icon' width='24' height='24'/>" >> $writefile
+			icontag="<img src='$icon' width='24' height='24'/>"
+			iconsbatch="$iconsbatch $icontag"
 		fi
 	done
+	echo $iconsbatch >> $writefile
 done
